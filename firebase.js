196f071c435619606058a938bf7ea3ac83e96975
falcon <alfxperson057@gmail.com>
2026-03-21 15:25:29 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+console.log("firebase kebaca");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAurXdX9sbPZupoBrz1m0S-ijE8ywJdnYg",
+  authDomain: "pengingat-servis-579a7.firebaseapp.com",
+  projectId: "pengingat-servis-579a7",
+  storageBucket: "pengingat-servis-579a7.firebasestorage.app",
+  messagingSenderId: "1070724674808",
+  appId: "1:1070724674808:web:72ad1d8bded088c7fdf70d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
